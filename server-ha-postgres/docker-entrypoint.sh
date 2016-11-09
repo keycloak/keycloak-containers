3c77c096402e76ae3fb03fc7c8895096fbcd8ba0
Staffan Olsson <staffan@repos.se>
2016-11-09 07:31:50 +0100

    Gives clustering a chance to work out of the box. Can be overridden by providing the same parameter again.

--- server-ha-postgres/docker-entrypoint.sh
+++ server-ha-postgres/docker-entrypoint.sh
@@ -8,5 +8,5 @@ export HOSTNAME_IP=$(hostname -i)
 export HOSTNAME_IP_ALL=$(hostname --all-ip-addresses)
 echo "hostname -i returned: $HOSTNAME_IP, -I returned: $HOSTNAME_IP_ALL"
 
-exec /opt/jboss/keycloak/bin/standalone.sh $@
+exec /opt/jboss/keycloak/bin/standalone.sh -Djboss.bind.address.private=$HOSTNAME_IP $@
 exit $?
