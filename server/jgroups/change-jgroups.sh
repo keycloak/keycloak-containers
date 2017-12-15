cee214de39d42d5749b4e48cd79c5c3859bab00e
Staffan Olsson <staffan@repos.se>
2017-12-15 14:01:51 +0100

    Removes a level of indirection, and lets us use vars in cli

--- server/jgroups/change-jgroups.sh
+++ server/jgroups/change-jgroups.sh
@@ -5,6 +5,7 @@ JGROUPS_DIR=$1
 cd /opt/jboss/keycloak
 
 if [ "$JGROUPS_DIR" == "TCPPING" ]; then
+
   [ -z "$JGROUPS_DNS_NAME" ] && export JGROUPS_DNS_NAME=jgroups-dns-ping
   while [ -z "$JGROUPS_INITIAL_HOSTS" ]; do
     sleep 1
@@ -12,11 +13,27 @@ if [ "$JGROUPS_DIR" == "TCPPING" ]; then
     getent hosts $JGROUPS_DNS_NAME | tee /tmp/hosts-$JGROUPS_DNS_NAME
     export JGROUPS_INITIAL_HOSTS=$(cat /tmp/hosts-$JGROUPS_DNS_NAME | awk '{ print $1 }' | paste -sd "," - )
   done
-  echo "[KEYCLOAK DOCKER IMAGE] Found initial JGroups TCPPING hosts: $JGROUPS_INITIAL_HOSTS"
-  # TODO how do we get variables into cli scripts? Some blog posts recommend --properties to jboss-cli.sh but I failed with that
-  export JGROUPS_INITIAL_HOSTS="$(echo $JGROUPS_INITIAL_HOSTS | sed 's/,/[7600],/g')[7600]"
-  sed "s|\${JGROUPS_INITIAL_HOSTS}|${JGROUPS_INITIAL_HOSTS}|" cli/jgroups/TCPPING/tcp-ping-initial-hosts.cli > /tmp/tcp-ping-initial-hosts.cli
-fi
 
-bin/jboss-cli.sh --file=cli/jgroups/$JGROUPS_DIR/standalone-ha-configuration.cli
-rm -rf standalone/configuration/standalone_xml_history/current/*
+  echo "[KEYCLOAK DOCKER IMAGE] Found initial JGroups TCPPING hosts: $JGROUPS_INITIAL_HOSTS"
+
+  export JGROUPS_INITIAL_HOSTS="$(echo $JGROUPS_INITIAL_HOSTS | sed 's/,/[7600],/g')[7600]"
+
+  bin/jboss-cli.sh <<__EOF
+    embed-server --server-config=standalone-ha.xml --std-out=echo
+    run-batch --file=cli/jgroups/tcp-stack.cli
+    run-batch --file=cli/jgroups/interface-private-eth0.cli
+    run-batch --file=cli/jgroups/mping-remove.cli
+    batch
+    /subsystem=jgroups/stack=tcp/protocol=TCPPING:add(add-index=0,properties=[ \
+      initial_hosts="${JGROUPS_INITIAL_HOSTS}" \
+    ])
+    run-batch
+    stop-embedded-server
+__EOF
+
+else
+
+  bin/jboss-cli.sh --file=cli/jgroups/$JGROUPS_DIR/standalone-ha-configuration.cli
+  rm -rf standalone/configuration/standalone_xml_history/current/*
+
+fi
