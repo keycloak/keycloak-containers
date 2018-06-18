87e91cc82796dcf60333dbce06b6f8372906e3b3
Stian Thorgersen <stian@redhat.com>
2018-06-18 17:26:53 +0200

    KEYCLOAK-7652 Add set-version script to Docker repository (#122)

---
+++ set-version.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+KEYCLOAK_VERSION=$1
+TAG_NAME=$2
+
+if [ "$KEYCLOAK_VERSION" == "" ] || [ "$TAG_NAME" == "" ]; then
+    echo "usage: set-version.sh <KEYCLOAK_VERSION> <TAG_NAME>"
+fi
+
+for i in adapter-wildfly examples proxy server; do
+	sed -i "s/ENV KEYCLOAK_VERSION .*/ENV KEYCLOAK_VERSION $KEYCLOAK_VERSION/" $i/Dockerfile
+done
+
+for i in server-mysql server-postgres server-openshift; do
+    sed -i "s/FROM jboss\/keycloak:.*/FROM jboss\/keycloak:$TAG_NAME/" $i/Dockerfile
+done
+
+for i in server-ha-postgres; do
+    sed -i "s/FROM jboss\/keycloak-postgres:.*/FROM jboss\/keycloak-postgres:$TAG_NAME/" $i/Dockerfile
+done
