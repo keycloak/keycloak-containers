8fdf752c4dad3ecbe74f43fa2fb33f1bf582a00c
Stian Thorgersen <stian@redhat.com>
2018-08-21 09:57:55 +0200

    KEYCLOAK-8072 Set permissions in format expected for OpenShift (#140)

--- server/tools/databases/change-database.sh
+++ server/tools/databases/change-database.sh
@@ -4,8 +4,8 @@ DB_VENDOR=$1
 
 cd /opt/jboss/keycloak
 
-bin/jboss-cli.sh --file=cli/databases/$DB_VENDOR/standalone-configuration.cli
+bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-configuration.cli
 rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
 
-bin/jboss-cli.sh --file=cli/databases/$DB_VENDOR/standalone-ha-configuration.cli
+bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-ha-configuration.cli
 rm -rf standalone/configuration/standalone_xml_history/current/*
