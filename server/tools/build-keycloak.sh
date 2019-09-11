77d4e5c8c51bc72b4bc8a9a01cb28c981063a95c
Sebastian Schuster <sebastian.schuster@bosch-si.com>
2019-09-11 16:26:12 +0200

    Used https to download MS SQL JDBC driver

--- server/tools/build-keycloak.sh
+++ server/tools/build-keycloak.sh
@@ -69,7 +69,7 @@ cp /opt/jboss/tools/databases/oracle/module.xml .
 
 mkdir -p /opt/jboss/keycloak/modules/system/layers/keycloak/com/microsoft/sqlserver/jdbc/main
 cd /opt/jboss/keycloak/modules/system/layers/keycloak/com/microsoft/sqlserver/jdbc/main
-curl -L http://central.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/$JDBC_MSSQL_VERSION/mssql-jdbc-$JDBC_MSSQL_VERSION.jar > mssql-jdbc.jar
+curl -L https://repo1.maven.org/maven2/com/microsoft/sqlserver/mssql-jdbc/$JDBC_MSSQL_VERSION/mssql-jdbc-$JDBC_MSSQL_VERSION.jar > mssql-jdbc.jar
 cp /opt/jboss/tools/databases/mssql/module.xml .
 
 ######################
