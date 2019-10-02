e409be28c194984d9f8c265bb71ad2435b77760e
Erik Lindskog <erik.lindskog@codemill.se>
2019-10-02 14:42:23 +0200

    Make the user/password handling more safe for whitespace in user or password

--- server/tools/docker-entrypoint.sh
+++ server/tools/docker-entrypoint.sh
@@ -33,8 +33,8 @@ file_env() {
 file_env 'KEYCLOAK_USER'
 file_env 'KEYCLOAK_PASSWORD'
 
-if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
-    /opt/jboss/keycloak/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
+if [ -n "$KEYCLOAK_USER" ] && [ -n "$KEYCLOAK_PASSWORD" ]; then
+    /opt/jboss/keycloak/bin/add-user-keycloak.sh --user "$KEYCLOAK_USER" --password "$KEYCLOAK_PASSWORD"
 fi
 
 ############
