b3d3015b04a1df4668d3304c77b838deb730fabc
stianst <stianst@gmail.com>
2020-04-30 14:24:36 +0200

    Fix build for 10

--- server/tools/build-keycloak.sh
+++ server/tools/build-keycloak.sh
@@ -33,7 +33,7 @@ if [ "$GIT_REPO" != "" ]; then
 
     tar xfz /opt/jboss/keycloak-source/distribution/server-dist/target/keycloak-*.tar.gz
     
-    mv /opt/jboss/keycloak-?.?.?* /opt/jboss/keycloak
+    mv /opt/jboss/keycloak-* /opt/jboss/keycloak
 
     # Remove temporary files
     rm -rf /opt/jboss/maven
