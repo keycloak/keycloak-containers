f8b7b2d2bf96ce388aa6ac2ceca6661b6100baad
Alex Szczuczko <aszczucz@redhat.com>
2020-05-21 13:40:08 -0600

    KEYCLOAK-13616 Also apply tech-preview to `name` label

--- keycloak-init-container/productize.sh
+++ keycloak-init-container/productize.sh
@@ -7,7 +7,7 @@ LABEL com.redhat.component="redhat-sso-7-sso74-tech-preview-init-container-rhel8
  io.k8s.description="Init Container for Red Hat SSO" \
  io.k8s.display-name="Red Hat SSO 7.4 Init Container" \
  io.openshift.tags="sso,sso74,keycloak,operator" \
- name="rh-sso-7\/sso74-init-container-rhel8" \
+ name="rh-sso-7\/sso74-init-container-rhel8-tech-preview" \
  maintainer="Red Hat Single Sign-On Team"
 END
 )
