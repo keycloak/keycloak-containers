729e939cb1f12e3a6a0fefec93e0cc95d1f95a02
Sebastian Laskawiec <slaskawi@redhat.com>
2020-05-26 10:54:18 +0200

    KEYCLOAK-13616 Update for delivery repos

--- keycloak-init-container/productize.sh
+++ keycloak-init-container/productize.sh
@@ -2,12 +2,13 @@
 
 LABELS=$(cat <<-END
 LABEL com.redhat.component="redhat-sso-7-sso74-tech-preview-init-container-rhel8-container" \
- description="Red Hat Single Sign-On 7.4 Init container image" summary="Red Hat Single Sign-On 7.4 Init container image" \
+ description="Red Hat Single Sign-On 7.4 Init container image" \
+ summary="Red Hat Single Sign-On 7.4 Init container image" \
  version="7.4" \
  io.k8s.description="Init Container for Red Hat SSO" \
  io.k8s.display-name="Red Hat SSO 7.4 Init Container" \
  io.openshift.tags="sso,sso74,keycloak,operator" \
- name="rh-sso-7\/sso74-init-container-rhel8-tech-preview" \
+ name="rh-sso-7-tech-preview\/sso74-init-container-rhel8" \
  maintainer="Red Hat Single Sign-On Team"
 END
 )
