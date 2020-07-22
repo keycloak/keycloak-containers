672fdfc0d8a17ed13599b4754b8514b481572e0b
stianst <stianst@gmail.com>
2020-07-22 15:41:32 +0200

    Remove deprecated images

--- set-version.sh
+++ set-version.sh
@@ -2,6 +2,4 @@
 
 KEYCLOAK_VERSION=$1
 
-for i in adapter-wildfly server gatekeeper; do
-	sed -i "s/ENV KEYCLOAK_VERSION .*/ENV KEYCLOAK_VERSION $KEYCLOAK_VERSION/" $i/Dockerfile
-done
+sed -i "s/ENV KEYCLOAK_VERSION .*/ENV KEYCLOAK_VERSION $KEYCLOAK_VERSION/" server/Dockerfile
