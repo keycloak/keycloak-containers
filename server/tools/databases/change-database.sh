d58c3d63d1298091c31eb838f16108cf21e77174
Aaron Kunde <aaron.kunde@adesso.de>
2021-10-28 10:44:37 +0200

    Make script simpler

--- server/tools/databases/change-database.sh
+++ server/tools/databases/change-database.sh
@@ -6,14 +6,11 @@ cd /opt/jboss/keycloak
 
 if [[ "$DB_VENDOR" == "oracle" && -n ${DB_SERVICE_NAME:-} ]]; then
     bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/oracle/standalone-configuration.service_name.cli
-else
-    bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-configuration.cli
-fi
-rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
-
-if [[ "$DB_VENDOR" == "oracle" && -n ${DB_SERVICE_NAME:-} ]]; then
     bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/oracle/standalone-ha-configuration.service_name.cli
 else
+    bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-configuration.cli
     bin/jboss-cli.sh --file=/opt/jboss/tools/cli/databases/$DB_VENDOR/standalone-ha-configuration.cli
 fi
+
+rm -rf /opt/jboss/keycloak/standalone/configuration/standalone_xml_history
 rm -rf standalone/configuration/standalone_xml_history/current/*
