5cc68611e91bce5336681d5e761ce0ead4a1a5cd
andreaTP <andrea.peruffo1982@gmail.com>
2021-11-19 11:39:26 +0000

    [KEYCLOAK-19831] Include latest Keycloak.X changes in the Docker image
    Co-authored-by: Dominik Guhr <dguhr@redhat.com>

--- server-x/tools/docker-entrypoint.sh
+++ server-x/tools/docker-entrypoint.sh
@@ -38,48 +38,6 @@ file_env 'KEYCLOAK_ADMIN_PASSWORD'
 # Start Keycloak #
 ##################
 
-CONFIG_ARGS=""
-RUN_CONFIG_START=false
-RUN_CONFIG=false
-SERVER_OPTS=""
-
-while [ "$#" -gt 0 ]
-do
-    case "$1" in
-      --auto-config)
-          RUN_CONFIG_START=true
-          ;;
-      config)
-          RUN_CONFIG=true
-          ;;
-      *)
-          if [[ $1 = --* || ! $1 =~ ^-.* ]]; then
-            CONFIG_ARGS="$CONFIG_ARGS $1"
-          else
-            SERVER_OPTS="$SERVER_OPTS $1"
-          fi
-          ;;
-    esac
-    shift
-done
-
-if [[ "$RUN_CONFIG_START" == true ]]; then
-  if [[ "$RUN_CONFIG" == true ]]; then
-    echo "ERROR: You can not run 'config' when passing the '--auto-config' start option.Please, choose one or another."
-    exit 2
-  fi
-
-  CONFIG_MARKER_FILE="/opt/jboss/keycloak/config_marker"
-
-  if [[ -n $CONFIG_ARGS && ! -f "$CONFIG_MARKER_FILE" ]]; then
-    exec /opt/jboss/keycloak/bin/kc.sh config $CONFIG_ARGS &
-    wait $!
-    touch $CONFIG_MARKER_FILE
-  fi
-
-  exec /opt/jboss/keycloak/bin/kc.sh $SERVER_OPTS
-else
-  exec /opt/jboss/keycloak/bin/kc.sh $SERVER_OPTS $CONFIG_ARGS
-fi
+exec /opt/keycloak/bin/kc.sh $@
 
 exit $?
