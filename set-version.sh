e57bc9b8d1c54fdaff94c421cc71c7840454524a
Stian Thorgersen <stianst@gmail.com>
2022-02-10 08:12:13 +0100

    Update set-version.sh

--- set-version.sh
+++ set-version.sh
@@ -2,6 +2,6 @@
 
 KEYCLOAK_VERSION=$1
 
-for target in 'server' 'server-x'; do
+for target in 'server'; do
     sed -i "s/ENV KEYCLOAK_VERSION .*/ENV KEYCLOAK_VERSION $KEYCLOAK_VERSION/" $target/Dockerfile
 done
